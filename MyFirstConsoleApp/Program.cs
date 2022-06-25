bd2b80aac3030d7e717f8dacf99d076ff96c682c
Adolfo Medina <a.star.for.me@hotmail.com>
2022-06-24 20:19:37 -0600

    Experimenting with Controls complete

--- MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/Program.cs
@@ -4,7 +4,25 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			OperatorExamples();
+		}
+
+		private static void OperatorExamples()
+		{
+			int width = 3;
+
+			width++;
+
+			int height = 2 + 4;
+			int area = width * height;
+			Console.WriteLine(area);
+
+			string result = "area";
+			result = result + " is " + area;
+			Console.WriteLine(result);
+
+			bool truthValue = true;
+			Console.WriteLine(truthValue);
 		}
 	}
 }
